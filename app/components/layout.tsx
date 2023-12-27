92b02dbbc8252ffd4f6c6bbcb3917f384018bb4f
ronak1603 <ronakm@gluelabs.com>
2023-12-27 15:31:57 +0530

    changes in layout component

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,6 +1,6 @@
 // app/components/layout.tsx
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-full bg-blue-600 font-mono">{children}</div>
+    <div className="h-screen w-full bg-blue-600 font-sans">{children}</div>
   );
 }
