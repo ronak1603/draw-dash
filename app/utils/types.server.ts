1f184678500b95bead6ec633a2506c45ac8ff65b
ronak1603 <ronakm@gluelabs.com>
2023-12-27 15:31:00 +0530

    validators and types file

---
+++ app/utils/types.server.ts
@@ -0,0 +1,10 @@
+export type RegisterForm = {
+  name: string;
+  userName: string;
+  password: string;
+};
+
+export type LoginForm = {
+  userName: string;
+  password: string;
+};
---
+++ app/utils/validators.server.ts
@@ -0,0 +1,16 @@
+const onValidUsername = (userName: string): string | undefined => {
+  const usernameRegex = /^[a-z0-9_.]+$/;
+  if (!usernameRegex.test(userName)) {
+    return "Please enter a valid email address";
+  }
+};
+
+export const validatePassword = (password: string): string | undefined => {
+  if (password.length < 5) {
+    return "Please enter a password that is at least 5 characters long";
+  }
+};
+
+export const validateName = (name: string): string | undefined => {
+  if (!name.length) return `Please enter a value`;
+};
