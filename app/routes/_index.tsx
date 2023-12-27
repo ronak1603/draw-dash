eb6dfe1c9abcb3068b0d43cc459e85f37008f24f
ronak1603 <ronakm@gluelabs.com>
2023-12-27 15:31:33 +0530

    routes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
+import { requireUserId } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+  return null;
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,32 +16,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      ronak
     </div>
   );
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,24 +1,123 @@
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+// app/routes/login.tsx
+import { useState } from "react";
+import { FormField } from "~/components/formField";
+import { Layout } from "~/components/layout";
+import { ActionFunction, json } from "@remix-run/node";
+import { validateName, validatePassword } from "~/utils/validators.server";
+import { login, register } from "~/utils/auth.server";
 
-import { db } from "~/utils/db.server";
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const action = form.get("_action");
+  const userName = form.get("userName");
+  const password = form.get("password");
+  const fullName = form.get("name");
 
-export const loader = async () => {
-  return json({
-    users: await db.user.findMany(),
-  });
+  if (
+    typeof action !== "string" ||
+    typeof userName !== "string" ||
+    typeof password !== "string"
+  ) {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+  }
+
+  if (action === "register" && typeof userName !== "string") {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+  }
+
+  const errors = {
+    userName: validateName(userName),
+    password: validatePassword(password),
+  };
+
+  if (Object.values(errors).some(Boolean))
+    return json(
+      { errors, fields: { userName, password }, form: action },
+      { status: 400 }
+    );
+
+  switch (action) {
+    case "login": {
+      return await login({ userName, password });
+    }
+    case "register": {
+      const name = fullName as string;
+
+      return await register({ userName, password, name });
+    }
+    default:
+      return json({ error: `Invalid Form Data` }, { status: 400 });
+  }
 };
 
-export default function Users() {
-  const data = useLoaderData<typeof loader>();
+export default function Login() {
+  const [action, setAction] = useState("login");
+
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+    name: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({ ...form, [field]: event.target.value }));
+  };
+
   return (
-    <>
-      <ul>
-        {data.users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <div>ronak</div>
-    </>
+    <Layout>
+      <div className="h-full justify-center items-center flex flex-col gap-y-4">
+        <h2 className="text-5xl font-extrabold text-yellow-300">
+          <button
+            onClick={() => setAction(action == "login" ? "register" : "login")}
+            className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+          >
+            {action === "login" ? "Sign Up" : "Sign In"}
+          </button>
+        </h2>
+        <p className="font-semibold text-slate-300">
+          {action === "login"
+            ? "Log In To Get Started!"
+            : "Sign Up To Get Started!"}
+        </p>
+
+        <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
+          {action === "register" && (
+            <FormField
+              htmlFor="name"
+              label="Name"
+              onChange={(e) => handleInputChange(e, "name")}
+              value={formData.name}
+            />
+          )}
+          <FormField
+            htmlFor="userName"
+            label="UserName"
+            value={formData.userName}
+            onChange={(e) => handleInputChange(e, "userName")}
+          />
+          <FormField
+            htmlFor="password"
+            type="password"
+            label="Password"
+            value={formData.password}
+            onChange={(e) => handleInputChange(e, "password")}
+          />
+
+          <div className="w-full text-center">
+            <button
+              type="submit"
+              name="_action"
+              value={action}
+              className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+            >
+              {action === "login" ? "Sign In" : "Sign Up"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 }
