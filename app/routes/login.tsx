bf448360d82ffdfc56c5fc3a0814c2e0d17d113d
ronak1603 <ronakm@gluelabs.com>
2024-01-01 23:06:37 +0530

    error handling in login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,63 +1,171 @@
-// app/routes/login.tsx
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+  ActionFunction,
+  ActionFunctionArgs,
+  LoaderFunction,
+  json,
+  redirect,
+} from "@remix-run/node";
+
 import { FormField } from "~/components/formField";
 import { Layout } from "~/components/layout";
-import { ActionFunction, json } from "@remix-run/node";
-import { validateName, validatePassword } from "~/utils/validators.server";
-import { login, register } from "~/utils/auth.server";
+import {
+  onValidUsername,
+  validateName,
+  validatePassword,
+} from "~/utils/validators.server";
+import { getUser, login, register } from "~/utils/auth.server";
+import { useActionData } from "@remix-run/react";
+import { badRequest } from "~/utils/request.server";
+import { prisma } from "~/utils/db.server";
 
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({
+  request,
+}: ActionFunctionArgs) => {
   const form = await request.formData();
   const action = form.get("_action");
   const userName = form.get("userName");
   const password = form.get("password");
   const fullName = form.get("name");
-
+  console.log("#### formdata", form);
   if (
     typeof action !== "string" ||
     typeof userName !== "string" ||
     typeof password !== "string"
   ) {
-    return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
   }
 
-  if (action === "register" && typeof userName !== "string") {
-    return json({ error: `Invalid Form Data`, form: action }, { status: 400 });
+  if (action === "register" && typeof fullName !== "string") {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
   }
 
-  const errors = {
-    userName: validateName(userName),
-    password: validatePassword(password),
+  console.log("#### after register");
+
+  const fields = {
+    action,
+    password,
+    fullName,
+    userName,
   };
 
-  if (Object.values(errors).some(Boolean))
-    return json(
-      { errors, fields: { userName, password }, form: action },
-      { status: 400 }
-    );
+  const fieldErrors = {
+    userName: onValidUsername(userName),
+    password: validatePassword(password),
+    ...(action === "register"
+      ? {
+          name: validateName((fullName as string) || ""),
+        }
+      : {}),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
 
   switch (action) {
     case "login": {
-      return await login({ userName, password });
+      const user = await login({ userName, password });
+      if (!user) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: "Username/Password combination is incorrect",
+        });
+      }
+
+      return user;
     }
     case "register": {
       const name = fullName as string;
 
-      return await register({ userName, password, name });
+      const userExists = await prisma.user.findFirst({
+        where: { userName },
+      });
+      if (userExists) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: `User with username ${userName} already exists`,
+        });
+      }
+      const user = await register({
+        userName,
+        password,
+        name,
+      });
+
+      if (!user) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: "Something went wrong trying to create a new user.",
+        });
+      }
+
+      return user;
     }
     default:
-      return json({ error: `Invalid Form Data` }, { status: 400 });
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: "Login type invalid",
+      });
   }
 };
 
+export const loader: LoaderFunction = async ({ request }) => {
+  return (await getUser(request)) ? redirect("/") : null;
+};
+
 export default function Login() {
-  const [action, setAction] = useState("login");
+  const actionData = useActionData<typeof action>();
+
+  console.log(actionData, " #### actionData");
+
+  const firstLoad = useRef(true);
+
+  const [formError, setFormError] = useState("");
 
   const [formData, setFormData] = useState({
     userName: "",
     password: "",
     name: "",
   });
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      const newState = {
+        name: "",
+        userName: "kody",
+        password: "ronak123",
+      };
+      setFormError("");
+      setFormData(newState);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      setFormError("");
+    }
+  }, [formData]);
+
+  useEffect(() => {
+    firstLoad.current = false;
+  }, []);
 
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -71,25 +179,31 @@ export default function Login() {
       <div className="h-full justify-center items-center flex flex-col gap-y-4">
         <h2 className="text-5xl font-extrabold text-yellow-300">
           <button
-            onClick={() => setAction(action == "login" ? "register" : "login")}
+            onClick={() =>
+              actionData.fields.action == "login" ? "register" : "login"
+            }
             className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
           >
-            {action === "login" ? "Sign Up" : "Sign In"}
+            {actionData.fields.action === "login" ? "Sign Up" : "Sign In"}
           </button>
         </h2>
         <p className="font-semibold text-slate-300">
-          {action === "login"
+          {actionData.fields.action === "login"
             ? "Log In To Get Started!"
             : "Sign Up To Get Started!"}
         </p>
 
         <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
-          {action === "register" && (
+          <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+            {formError}
+          </div>
+          {actionData.fields.action === "register" && (
             <FormField
               htmlFor="name"
               label="Name"
               onChange={(e) => handleInputChange(e, "name")}
               value={formData.name}
+              error={actionData.fieldErrors.name}
             />
           )}
           <FormField
@@ -97,6 +211,7 @@ export default function Login() {
             label="UserName"
             value={formData.userName}
             onChange={(e) => handleInputChange(e, "userName")}
+            error={actionData.fieldErrors.userName}
           />
           <FormField
             htmlFor="password"
@@ -104,16 +219,17 @@ export default function Login() {
             label="Password"
             value={formData.password}
             onChange={(e) => handleInputChange(e, "password")}
+            error={actionData.fieldErrors.password}
           />
 
           <div className="w-full text-center">
             <button
               type="submit"
               name="_action"
-              value={action}
+              value={actionData.fields.action}
               className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
             >
-              {action === "login" ? "Sign In" : "Sign Up"}
+              {actionData.fields.action === "login" ? "Sign In" : "Sign Up"}
             </button>
           </div>
         </form>
