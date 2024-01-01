a1801ce1a572fce5c469e4ef7e8752ed99cdb293
ronak1603 <ronakm@gluelabs.com>
2024-01-01 23:05:58 +0530

    add error type in component

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,4 +1,3 @@
-// app/components/layout.tsx
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen w-full bg-blue-600 font-sans">{children}</div>
