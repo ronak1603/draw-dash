e4ddc9fa3572e512f9126e210b7916d5b3dd4994
ronak1603 <ronakm@gluelabs.com>
2024-01-02 13:20:44 +0530

    add drawing model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,47 +9,6 @@ async function seed() {
       password: "$2y$10$GcK1J9Zs6D48Fvjx0R0E4eNif/e5WNSayx1AIJHmRutUk6aKNoO8q",
     },
   });
-  await Promise.all(
-    getJokes().map((joke) => {
-      const data = { jokesterId: kody.id, ...joke };
-      return db.joke.create({ data });
-    })
-  );
 }
 
-seed(); //npx prisma db seed //npx prisma db push //prsima db pull //prisma generate //npx prisma studio
-
-function getJokes() {
-  // shout-out to https://icanhazdadjoke.com/
-
-  return [
-    {
-      name: "Road worker",
-      content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`,
-    },
-    {
-      name: "Frisbee",
-      content: `I was wondering why the frisbee was getting bigger, then it hit me.`,
-    },
-    {
-      name: "Trees",
-      content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`,
-    },
-    {
-      name: "Skeletons",
-      content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`,
-    },
-    {
-      name: "Hippos",
-      content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`,
-    },
-    {
-      name: "Dinner",
-      content: `What did one plate say to the other plate? Dinner is on me!`,
-    },
-    {
-      name: "Elevator",
-      content: `My first time using an elevator was an uplifting experience. The second time let me down.`,
-    },
-  ];
-}
+seed(); //npx prisma db push //npx prisma db seed //prisma db pull //prisma generate //npx prisma studio
